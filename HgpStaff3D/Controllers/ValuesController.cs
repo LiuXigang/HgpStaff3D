1a13203ffab5f9ef442beb9f68d9bc3e6fcf9e0f
榴莲 <1436215718@qq.com>
2019-09-22 23:51:16 +0800

    HttpClientFactory Polly

---
+++ HgpStaff3D/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HgpStaff3D.Controllers
+{
+    public class ValuesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ValuesController(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            ViewBag.Msg = await client.GetStringAsync("https://www.microsoft.com/zh-cn/"); ;
+            return View();
+        }
+
+        public async Task<ActionResult<string>> Get()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "repos/aspnet/docs/pulls");
+            var client = _httpClientFactory.CreateClient("github");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
+}
--- HgpStaff3D/Startup.cs
+++ HgpStaff3D/Startup.cs
@@ -11,9 +11,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.IO;
+using System.Net.Http;
 
 namespace HgpStaff3D
 {
@@ -70,31 +72,62 @@ namespace HgpStaff3D
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, "HgpStaff3D.XML");
                 c.IncludeXmlComments(xmlPath);
             });
+            #region HttpClient
+            services.AddHttpClient();
+            services.AddHttpClient("github", c =>
+            {
+                c.BaseAddress = new Uri("https://api.github.com/");
+                c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600))
+            //重试3次，每次600ms
+            );
+            services.AddHttpClient("test", c =>
+            {
+                c.BaseAddress = new Uri("http://localhost:5000/");
+                c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            })
+    // 熔断
+    .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<Exception>().CircuitBreakerAsync(2, TimeSpan.FromSeconds(4), (ex, ts) =>
+    {
+        Console.WriteLine($"break here {ts.TotalMilliseconds}");
+    }, () =>
+    {
+        Console.WriteLine($"reset here ");
+    }))
+    // 超时
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+        #endregion
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
-
-            app.UseSwagger(c => { c.RouteTemplate = "swagger/{documentName}/swagger.json"; });
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("v1/swagger.json", "HgpStaff3D");
-            });
-        }
     }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCookiePolicy();
+
+        app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
+
+        app.UseSwagger(c => { c.RouteTemplate = "swagger/{documentName}/swagger.json"; });
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("v1/swagger.json", "HgpStaff3D");
+        });
+    }
+}
 }
